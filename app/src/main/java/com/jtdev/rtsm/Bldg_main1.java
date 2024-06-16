119d54f599e7cd702784e48d36e23c7bd7c95b12
Reddjoseph <126237302+Reddjoseph@users.noreply.github.com>
2024-06-16 15:14:39 +0800

    GG

--- app/src/main/java/com/jtdev/rtsm/Bldg_main1.java
+++ app/src/main/java/com/jtdev/rtsm/Bldg_main1.java
@@ -4,57 +4,87 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
 public class Bldg_main1 extends AppCompatActivity {
 
-    ImageView lab3;
+    ImageView travel1, travel2, comlab3;
+    TextView comlab3_status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bldg_main1);
 
-        Date currentTime = Calendar.getInstance().getTime();
-        TimeZone pstTimeZone = TimeZone.getTimeZone("GMT+8:00");
-        Calendar calendar = Calendar.getInstance(pstTimeZone);
-        calendar.setTime(currentTime);
+        travel1 = findViewById(R.id.travel1);
+        travel2 = findViewById(R.id.travel2);
+        comlab3 = findViewById(R.id.Comlab3_Button);
 
-        Button Floor2_1 = findViewById(R.id.Floor2_1);
-        Button Floor2_2 = findViewById(R.id.Floor2_2);
-        lab3 = findViewById(R.id.ComLab3Button);
-        Floor2_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Bldg_main1.this, bldg_main2.class);
-                startActivity(intent);
-            }
-        });
+        comlab3_status = findViewById(R.id.Comlab3_Status);
 
-        Floor2_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Bldg_main1.this, bldg_main2.class);
-                startActivity(intent);
-            }
-        });
-    lab3.setOnClickListener(new View.OnClickListener() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR); // 12-hour format
+        int minute = calendar.get(Calendar.MINUTE);
+        int am_pm = calendar.get(Calendar.AM_PM);
+
+        String amPm;
+        if (am_pm == Calendar.AM) {
+            amPm = "AM";
+        } else {
+            amPm = "PM";
+        }
+
+        String currentTime = (hour + ":" + minute + " " + amPm);
+
+    comlab3.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            if (compareTime(12, 13)){
+                Toast.makeText(Bldg_main1.this, "This room is currently VACANT", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(Bldg_main1.this, "A class is ongoing in this room.", Toast.LENGTH_SHORT).show();
+            }
             AlertDialog.Builder comlab3Message = new AlertDialog.Builder(Bldg_main1.this);
             comlab3Message.setTitle("Computer Laboratory 3");
-            comlab3Message.setMessage("Schedule: " + calendar.getTime());
+            StringBuilder comlab3_content = new StringBuilder();
+            comlab3_content.append("Schedule                           SUBJECT\n");
+            comlab3_content.append("7:00AM - 10:00AM          HCI\n");
+            comlab3_content.append("10:00AM - 11:00AM        HCI\n");
+            comlab3_content.append("12:00AM - 1:00PM          VACANT\n");
+            comlab3_content.append("1:00AM - 4:00PM             FMP\n");
+            comlab3_content.append("4:00AM - 6:00PM             FMP\n");
+            comlab3_content.append("\nCurrent Time: ");
+            comlab3_content.append(currentTime);
+            comlab3Message.setMessage(comlab3_content);
             comlab3Message.show();
 
         }
     });
 
+    updateTime();
+    }
+    public boolean compareTime(int time1, int time2) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY); // 24-hour format
 
+        return hour >= time1 && hour < time2;
+    }
+
+    void updateTime(){
+        if (compareTime(12, 13)){
+            comlab3_status.setText("Available");
+            comlab3_status.setTextColor(Color.GREEN);
+        }else{
+            comlab3_status.setText("Occupied");
+            comlab3_status.setTextColor(Color.RED);
+        }
     }
 }
--- app/src/main/java/com/jtdev/rtsm/login_system.java
+++ app/src/main/java/com/jtdev/rtsm/login_system.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class login_system extends AppCompatActivity {
 
     @Override
@@ -14,6 +18,12 @@ public class login_system extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_system);
         Button login = findViewById(R.id.btnLogin);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Accounts");
+
+        myRef.setValue("RedPogi 123MahalKoSiRed");
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
